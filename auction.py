46ade1ae7976870bba8e8ba719233aa435e251ae
Akash Velu <akashvelu@DN0a180651.SUNet>
2022-05-22 15:02:56 -0700

    first commit

---
+++ auction.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+class Auction(object):
+    def __init__(self, n_teams, n_players, player_values, players_per_team, reserve_price):
+        self.n_teams = n_teams
+        self.n_players = n_players
+
+        self.players_per_team = players_per_team
+        self.reserve_price = reserve_price
+
+        # in the order in which they are presented.
+        self.player_values = player_values
+
+        self.state_dim = len(self.player_values) + 1
+
+        # variables for maintaining auction state
+        self.auction_idx = 0
+        self.team_players = {i: [] for i in range(self.n_teams)}
+        self.value_vector = np.array(self.player_values)
+        self.team_completed = np.zeros(self.n_teams)
+        self.auction_history = []
+
+    def reset(self):
+        self.auction_idx = 0
+        self.team_players = {i: [] for i in range(self.n_teams)}
+        self.value_vector = np.array(self.player_values)
+        self.team_completed = np.zeros(self.n_teams)
+        self.auction_history = []
+        return np.stack([self.get_auction_representation(i) for i in range(self.n_teams)])
+
+    def get_auction_representation(self, team_idx):
+        players_left_to_buy = self.players_per_team - len(self.team_players[team_idx])
+        return np.concatenate([np.array([players_left_to_buy]), self.value_vector], axis=-1).astype(np.float32)
+
+    def step(self, team_bids):
+        # sort the bids, determine the winning team assign player to team and get the 2nd highest bid as price
+        # assert np.min(team_bids) >= self.reserve_price
+        team_bids[self.team_completed == 1.0] = -np.inf
+        argsorted_bids = np.argsort(team_bids)
+        winning_team = argsorted_bids[-1]
+        self.team_players[winning_team].append(self.auction_idx)
+        price = team_bids[argsorted_bids[-2]] if not abs(team_bids[argsorted_bids[-2]]) == np.inf else self.reserve_price
+
+        # assign reward of 0 to all teams who didn't win the player
+        payoff = self.player_values[self.auction_idx] - price
+        rewards = np.zeros(self.n_teams)
+        rewards[winning_team] = payoff
+
+        # update the state of the auction
+        self.value_vector[self.auction_idx] = -1.0
+
+        if len(self.team_players[winning_team]) == self.players_per_team:
+            self.team_completed[winning_team] = 1.0
+
+        states = [self.get_auction_representation(i) for i in range(self.n_teams)]
+        self.auction_history.append((team_bids, winning_team, price, payoff))
+        done = np.all(self.team_completed)
+        self.auction_idx += 1
+        return np.stack(states), rewards.reshape(-1, 1), self.team_completed.reshape(-1, 1).copy(), done
---
+++ auction_train.py
@@ -0,0 +1,132 @@
+import numpy as np
+import torch
+from torch.distributions import Normal
+from auction import Auction
+
+
+class PolicyNetwork(torch.nn.Module):
+    def __init__(self, in_features, reserve_price, hidden_size=32, init_std=1.0):
+        super(PolicyNetwork, self).__init__()
+        self.fc1 = torch.nn.Linear(in_features, out_features=hidden_size)
+        self.fc2 = torch.nn.Linear(in_features=hidden_size, out_features=1, bias=False)
+        self.reserve_price = reserve_price
+        self.log_std = torch.nn.Parameter(torch.log(torch.from_numpy(np.array([init_std]))))
+        self.log_std.requires_grad = True
+
+    def forward(self, x):
+        hidden = torch.nn.functional.relu(self.fc1(x))
+        out = torch.exp(self.fc2(hidden))
+        mean = out + self.reserve_price
+        dist = Normal(loc=mean, scale=torch.exp(self.log_std))
+        return dist
+
+    def get_action(self, state):
+        # state should be B, D
+        dist = self.forward(state)
+        sample = dist.sample()
+        mode = dist.mean
+        sample_log_prob = dist.log_prob(sample)
+        mode_log_prob = dist.log_prob(mode)
+        return sample, mode, sample_log_prob, mode_log_prob
+
+
+class Trainer:
+    def __init__(self, auction, policy, lr, num_train_steps, episodes_per_update, gamma=0.99):
+        self.auction = auction
+        self.policy = policy
+        self.lr = lr
+        self.num_train_steps = num_train_steps
+        self.episodes_per_update = episodes_per_update
+        self.gamma = gamma
+        self.optimizer = torch.optim.Adam(lr=lr, params=self.policy.parameters())
+
+    def rollout_auction(self, use_greedy=False):
+        states = self.auction.reset()
+        done = False
+        states_list, bids_list, logprob_list, rewards_list, dones_list = [states], [], [], [], [
+            np.zeros((self.auction.n_teams, 1))]
+        while not done:
+            bids_sampled, bids_mean, sample_logprob, mean_logprob = self.policy.get_action(torch.from_numpy(states))
+            if use_greedy:
+                bids = bids_mean.detach().numpy()
+                logprob = mean_logprob
+            else:
+                bids = bids_sampled.detach().numpy()
+                logprob = sample_logprob
+            states, rewards, team_dones, done = self.auction.step(bids.flatten())
+
+            states_list.append(states)
+            bids_list.append(bids)
+            logprob_list.append(logprob)
+            rewards_list.append(rewards)
+            dones_list.append(team_dones)
+        # H, N, D
+        return np.stack(states_list), np.stack(bids_list), torch.stack(logprob_list), np.stack(rewards_list), np.stack(
+            dones_list)
+
+
+    def train(self):
+        for i in range(self.num_train_steps):
+            loss = self.train_step()
+            print("loss at iteration " + str(i) + ": " + str(loss))
+            _, bids, _, rewards, dones = self.rollout_auction(use_greedy=True)
+            team_returns = rewards.sum(0).flatten()
+            print("Eval team returns: ", team_returns)
+            print("")
+
+    def train_step(self):
+        # rollout
+        rollouts = [self.rollout_auction() for _ in range(self.episodes_per_update)]
+        # B*N, H, D shape
+        logprobs_batch = collapse_horizon(torch.stack([rollout[2] for rollout in rollouts]))
+        rewards_batch = collapse_horizon(np.stack([rollout[3] for rollout in rollouts]))
+        # B*N, H + 1, D; ith horizon index indicates if the i+1th state is "out of bounds"
+        dones_batch = collapse_horizon(np.stack([rollout[4] for rollout in rollouts]))
+        rewards_to_go = self.get_rewards_to_go(rewards_batch, dones_batch[:, 1:])
+        rewards_to_go = torch.from_numpy(rewards_to_go)
+
+        loss = (-1 * rewards_to_go * logprobs_batch * torch.from_numpy(dones_batch[:, :-1])).mean()
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        return loss
+
+    def get_rewards_to_go(self, rewards, dones, normalize=True):
+        # shape is BS, H, 1
+        rewards_to_go = np.zeros_like(rewards)
+        BS, H, _ = rewards_to_go.shape
+        R = np.zeros((BS, 1))
+        for i in reversed(range(H)):
+            R = rewards[:, i] + self.gamma * (1 - dones[:, i]) * R
+            rewards_to_go[:, i] = R
+        mean = rewards_to_go.mean()
+        std = rewards_to_go.std()
+        if normalize:
+            return (rewards_to_go - mean) / (std + 1e-9)
+        else:
+            return rewards_to_go
+
+def collapse_horizon(arr):
+    # B, H, N, D -> B*N, H, D
+    B, H, N, D = arr.shape
+    if type(arr) == np.ndarray:
+        arr = np.moveaxis(arr, 2, 1)
+        return arr.reshape(B * N, H, D)
+    else:
+        arr = torch.moveaxis(arr, 2, 1)
+        return arr.reshape(B * N, H, D)
+
+
+if __name__ == '__main__':
+
+    auction = Auction(n_teams=2, n_players=4, player_values=[4, 3, 2, 1], players_per_team=2, reserve_price=0)
+    policy = PolicyNetwork(auction.state_dim, auction.reserve_price)
+
+
+    # states, bids, rewards, dones = rollout_auction(policy, auction)
+    trainer = Trainer(auction, policy, 3e-3, 1000, 100)
+    trainer.train()
+    a = 2
+
+
+
